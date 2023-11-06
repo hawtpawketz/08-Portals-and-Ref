066245abf9cbf265d7194a024b382bc2794bbb79
hawtpawketz <145611129+hawtpawketz@users.noreply.github.com>
2023-11-06 15:37:25 -0600

    Added Challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChllenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChllenge title="Easy" targetTime={1} />
+        <TimerChllenge title="Not Easy" targetTime={5} />
+        <TimerChllenge title="Getting Tough" targetTime={10} />
+        <TimerChllenge title="Pros Only" targetTime={15} />
+      </div>
     </>
   );
 }
